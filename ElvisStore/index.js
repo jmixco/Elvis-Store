7510ee4efd02d7f3f76f76c57d42ebb99dd657f0
Julio Mixco <jmixco@applaudostudios.com>
2017-03-21 18:07:07 -0600

    -- interfaz de busqueda

--- ElvisStore/index.js
+++ ElvisStore/index.js
@@ -5,10 +5,17 @@ const app = express();
 const appmodules = require('./app');
 const passport = require('passport');
 
+app.set('port', process.env.PORT || 3000);
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+    res.render('index')
+});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.set('port', process.env.PORT || 3000);
+
+
 // Initialize passport for use
 app.use(passport.initialize());
 
